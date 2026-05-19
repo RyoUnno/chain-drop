1d11291b164637cbf2f66d483f397c85fdea1b5d
RyoUnno <ryo.unno1208@gmail.com>
2026-05-20 00:44:03 +0900

    Add block asset config

---
+++ block-assets.js
@@ -0,0 +1,34 @@
+// Swap block art here. Put image files in assets/blocks/ and set image to the path.
+// Leave image as "" to use the built-in accessible color + symbol block.
+window.CHAIN_DROP_BLOCKS = [
+  {
+    id: "red",
+    label: "slash",
+    image: "",
+  },
+  {
+    id: "blue",
+    label: "circle",
+    image: "",
+  },
+  {
+    id: "green",
+    label: "cross",
+    image: "",
+  },
+  {
+    id: "yellow",
+    label: "square",
+    image: "",
+  },
+  {
+    id: "violet",
+    label: "diamond",
+    image: "",
+  },
+  {
+    id: "coral",
+    label: "triangle",
+    image: "",
+  },
+];
