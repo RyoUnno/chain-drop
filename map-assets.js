13f066f91fc8067ed34dbd077269cb54b80427ec
RyoUnno <ryo.unno1208@gmail.com>
2026-05-21 13:27:35 +0900

    feat: add stage map asset config

---
+++ map-assets.js
@@ -0,0 +1,19 @@
+(function () {
+  var basePath = "assets/maps/";
+
+  window.CHAIN_DROP_STAGE_MAP = {
+    basePath: basePath,
+    background: "default-map.svg",
+    aspectRatio: "1 / 1.18",
+    markerImage: "",
+    clearedMarkerImage: "",
+    lockedMarkerImage: "",
+    nodes: [
+      { x: 18, y: 80 },
+      { x: 38, y: 66 },
+      { x: 28, y: 45 },
+      { x: 58, y: 34 },
+      { x: 77, y: 18 },
+    ],
+  };
+})();
